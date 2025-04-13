15975ecf45326902dde90da530593e1763a18f6b
olavrenin-data-scientist <olavrenin@berkeley.edu>
2025-04-13 12:06:39 -0700

    Added football code.

--- temp_merge_script.py
+++ temp_merge_script.py
@@ -1,8 +1,8 @@
 import nbformat
 
 # Load both notebooks
-nb1 = nbformat.read("Oleksii.ipynb", as_version=4)
-nb2 = nbformat.read("Jimmy.ipynb", as_version=4)
+nb1 = nbformat.read("2parts.ipynb", as_version=4)
+nb2 = nbformat.read("Nana.ipynb", as_version=4)
 
 # Combine
 nb1.cells.extend(nb2.cells)
